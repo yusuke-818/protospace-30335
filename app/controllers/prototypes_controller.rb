fe54561d98cbe264de1efc48ce7f6020e90d6c89
yusuke-818 <pokko.pen@icloud.com>
2021-01-16 17:03:33 +0900

    before_actionの設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
@@ -20,17 +21,15 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
+    
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
@@ -39,7 +38,6 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
     if @prototype.destroy
       redirect_to root_path
     else
@@ -49,14 +47,16 @@ class PrototypesController < ApplicationController
 
   private
 
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
   def contributor_confirmation
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+    redirect_to root_path unless current_user == @prototype.user
   end
 
 end
